7f4535fa4e179d3ddc5a5637f62d5d45627fd2d7
Matthew Manalili <matthewmanalili@gmail.com>
2024-05-14 11:41:50 +0800

    Added updated files

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+const MainLayout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+
+export default MainLayout;
