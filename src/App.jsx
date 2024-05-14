9c38a02d9edeeedf96a9619448d87efdcde0146a
Matthew Manalili <matthewmanalili@gmail.com>
2024-05-14 18:26:15 +0800

    Added updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ import NotFoundPage from "./pages/NotFoundPage";
 import NotesPage from "./pages/NotesPage";
 import AddNotesPage from "./pages/AddNotesPage";
 import HomePage from "./pages/HomePage";
-import NotePage, {noteLoader} from "./pages/Notepage"
+import NotePage, { noteLoader } from "./pages/Notepage";
 import { NotesProvider } from "./contexts/NoteContext";
 import EditNotesPage from "./pages/EditNotesPage";
+import Notepage from "./pages/Notepage";
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -19,14 +20,10 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="/notes" element={<NotesPage />} />
         <Route path="/add-notes" element={<AddNotesPage />} />
+        <Route path="/notes/:id" element={<Notepage />} loader={noteLoader} />
         <Route
-          path="/notes/:id"
-          element={<NotePage  />}
-          loader={noteLoader}
-        />
-         <Route
           path="/edit-note/:id"
-          element={<EditNotesPage  />}
+          element={<EditNotesPage />}
           loader={noteLoader}
         />
         <Route path="*" element={<NotFoundPage />} />
